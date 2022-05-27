f90813e5e36eb6ae1f4770f07dce91af3f9611f5
Josue Lubaki <josuelubaki@gmail.com>
2022-05-26 21:52:37 -0400

    config scan

--- src/main/java/ca/josue/cleanme/CleanMeApplication.java
+++ src/main/java/ca/josue/cleanme/CleanMeApplication.java
@@ -2,8 +2,10 @@ package ca.josue.cleanme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CleanMeApplication {
 
     public static void main(String[] args) {
